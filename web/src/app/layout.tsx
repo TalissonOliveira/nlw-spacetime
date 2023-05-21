3d6434c361238818493e5cd81f34f392b8f51de2
Talisson Oliveira <talissonlux@outlook.com>
2023-05-20 23:07:36 -0300

    feat(web): initial setup

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,37 @@
+import { ReactNode } from 'react'
+import './globals.css'
+import {
+  Roboto_Flex as Roboto,
+  Bai_Jamjuree as BaiJamjuree,
+} from 'next/font/google'
+
+const roboto = Roboto({
+  subsets: ['latin'],
+  variable: '--font-roboto',
+  display: 'swap',
+})
+
+const baiJamjuree = BaiJamjuree({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: '700',
+  variable: '--font-bai-jamjuree',
+})
+
+export const metadata = {
+  title: 'Cápsula do Tempo | Spacetime',
+  description:
+    'Uma cápsula do tempo construída com React, Next.js, TailwindCSS e TypeScript.',
+}
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${roboto.variable} ${baiJamjuree.variable} bg-gray-900 font-sans text-gray-100`}
+      >
+        {children}
+      </body>
+    </html>
+  )
+}
